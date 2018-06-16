cfd2d9d468f4f10bcffdc93cc9fe33f93f43fc5f
xXxruofxXx <leon.mueller@freenet.de>
2018-06-17 01:32:58 +0200

    Health_Bar

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour {
+
+    public float health = 1.0f;
+    
+
+    
+
+
+	void Update () {
+
+        Image image = GetComponent<Image>();
+        image.fillAmount = health;
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            health = 0.6f;
+        }
+    }
+}
