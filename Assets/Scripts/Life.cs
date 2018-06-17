7ac5dd2126130a66b39b58ddae93f37e25fe393e
xXxruofxXx <leon.mueller@freenet.de>
2018-06-17 15:52:48 +0200

    Herz

---
+++ Assets/Scripts/Life.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Life : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D collidedObject)
+    {
+        switch (collidedObject.tag)
+        {
+            case "Player":
+                HealthBar.health += 0.1f;
+
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
