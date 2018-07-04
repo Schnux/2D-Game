f6311e0b1412343c748b484fe5f45095c2720b19
xXxruofxXx <leon.mueller@freenet.de>
2018-07-04 19:29:48 +0200

    Enemy funktioniert noch nicht ganz

---
+++ Assets/Scripts/PlayerE.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerE : MonoBehaviour {
+
+    #region Singelton
+
+    public static PlayerE instance;
+    
+    void Awake ()
+    {
+        instance = this;
+    }
+
+    #endregion
+
+    public GameObject Player;
+}
