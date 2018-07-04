4bee51fd1306a9a7037ccd131bb26edc94a1815b
xXxruofxXx <leon.mueller@freenet.de>
2018-07-04 17:54:25 +0200

    Enemy

---
+++ Assets/Scripts/Enemy_Controller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.AI;
+
+public class Enemy_Controller : MonoBehaviour {
+
+    public float lookRadius = 10f;
+
+  //  Transform target;
+    //NavMeshAgent agent;
+
+	void Start ()
+    { 
+
+      //  agent = GetComponent<NavMeshAgent>();
+	}
+
+	void Update () {
+		
+	}
+
+    void OnDrawGizmosSelected ()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+
+}
