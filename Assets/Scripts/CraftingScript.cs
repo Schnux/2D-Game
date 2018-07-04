110cad343917ae2019b12c83a60917237f91e27b
Fabi-M <40360223+Fabi-M@users.noreply.github.com>
2018-07-04 16:33:48 +0200

    craftingscriptweiter

--- Assets/Scripts/CraftingScript.cs
+++ Assets/Scripts/CraftingScript.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CraftingScript : MonoBehaviour {
+    private Inventory inventory;
+    public InventoryItemBase item;
+    // Use this for initialization
+    void Start() {
+        
+        inventory = FindObjectOfType<Inventory>();
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Update is called once per frame
+    void Update() {
+
+    }
+    public void Craften()
+    {
+        item = FindObjectOfType<Copper_Ore>();
+        inventory.AddItem(item);
+    }
+    public void Craftenzwei()
+    {
+        item = FindObjectOfType<Iron_Ore>();
+        inventory.AddItem(item);
+    }
 }
