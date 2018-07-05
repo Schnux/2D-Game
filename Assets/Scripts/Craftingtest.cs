fb61945e5cb169ae4b0e04b23b68d106e2799f21
Fabi-M <40360223+Fabi-M@users.noreply.github.com>
2018-07-05 15:43:57 +0200

    craftingweiter

---
+++ Assets/Scripts/Craftingtest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Craftingtest : MonoBehaviour
+{
+
+    public GameObject Panel;
+    int offen;
+
+    public void Update()
+    {
+        
+
+    }
+    public void hideshowPanel()
+    {
+
+
+        if (offen % 2 == 1)
+        { Panel.gameObject.SetActive(false); }
+        else
+        { Panel.gameObject.SetActive(true); }
+
+        offen++;
+
+    }
+}
