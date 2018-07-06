4c87c9b9a651c2a159f54a97582a6885fdc2a3aa
FL0777 <38884935+FL0777@users.noreply.github.com>
2018-07-06 16:07:36 +0200

    Items added
    weitere hinzugefügt

---
+++ Assets/Scripts/Block_struct/iron_ingot.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class iron_ingot : InventoryItemBase
+{
+
+    public override void OnUse()
+    {
+        // TODO: Do something with the object.... next tutorial!
+        base.OnUse();
+
+    }
+}
