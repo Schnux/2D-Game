0990f55c2aba71d9f81b066249f9acb797355764
Fabi-M <40360223+Fabi-M@users.noreply.github.com>
2018-07-08 18:19:33 +0200

    fuerflo

---
+++ Assets/Scripts/Testfuerflo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testfuerflo : MonoBehaviour {
+
+    public GameObject Anleitung;
+    private bool offen = false;
+    public void anleitungzeigen()
+    {
+
+
+        if (offen == true)
+        { Anleitung.gameObject.SetActive(false);
+            offen = false;
+        }
+        else
+        { Anleitung.gameObject.SetActive(true);
+            offen = true;
+        }
+
+        
+    }
+}
