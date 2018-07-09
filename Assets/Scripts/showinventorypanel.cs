e7f5f8991b96bbf0b0d902110341a3e89b6929dc
FL0777 <38884935+FL0777@users.noreply.github.com>
2018-07-09 12:30:16 +0200

    Oberfläche
    Inventar aus bzw. einblenden

---
+++ Assets/Scripts/showinventorypanel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class showinventorypanel : MonoBehaviour
+{
+
+    public GameObject InventoryPanel;
+    int counter = 1;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+            showhidePanel();
+
+    }
+    public void showhidePanel()
+    {
+
+
+        if (counter % 2 == 1)
+        { InventoryPanel.gameObject.SetActive(false); }
+        else
+        { InventoryPanel.gameObject.SetActive(true); }
+
+        counter++;
+
+    }
+}
+
